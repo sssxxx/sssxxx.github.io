8ac2994abcfe91291936a9413ef40f8a2f8f12f2
suixin <1987629290@qq.com>
2022-07-10 23:23:56 +0800

    'feat：测试'

---
+++ js/px2rem.js
@@ -0,0 +1,13 @@
+var deviceWidth = document.documentElement.clientWidth;
+
+if (deviceWidth > 750) deviceWidth = 750;
+
+document.documentElement.style.fontSize = deviceWidth / 7.5 + "px";
+
+window.onresize = function () {
+  var deviceWidth = document.documentElement.clientWidth;
+
+  if (deviceWidth > 750) deviceWidth = 750;
+
+  document.documentElement.style.fontSize = deviceWidth / 7.5 + "px";
+};
